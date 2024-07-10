2ef463dfba35ee5b5f5361fadcdf747ef479e2a4
Ubaidahmed221 <ubaidahmed1282@gmail.com>
2024-07-10 23:28:06 +0500

    project set up

--- BirdSalesApi/BirdSalesWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BirdSalesWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
